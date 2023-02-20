04d78584610ad67cdd97e0b31cf49f0838677faa
tushar <tushar@pop-os.localdomain>
2023-02-21 02:32:42 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -15,14 +15,14 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://roomdev.netlify.app/"],
+    origin: ["https://roomdev.netlify.app/", "http://localhost:5173"],
   })
 );
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://roomdev.netlify.app/"],
+    origin: ["https://roomdev.netlify.app/", "http://localhost:5173"],
     methods: "*",
   },
 });
